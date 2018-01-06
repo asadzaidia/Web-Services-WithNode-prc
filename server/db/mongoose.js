d17ec199130c0bd48874bf8d11630b121cdff4b0
asadzaidia <asadzaidi625@gmail.com>
2018-01-06 15:29:51 +0500

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://<asad>:<asad>@ds047945.mlab.com:47945/todoapi' );
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoAppApi' );
 //as mongoose by default uses callbacks but we are using here promises so set promises global for mongoose
 
 module.exports={mongoose};
