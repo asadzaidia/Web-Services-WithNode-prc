eafd20c10e4cc809923c46be54aa2942ba4b1228
asadzaidia <asadzaidi625@gmail.com>
2018-01-06 13:42:13 +0500

    y

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://<asad>:<asad>@ds047945.mlab.com:47945/todoapi');
+mongoose.connect('heroku config:set PROD_MONGODB=mongodb://<asad>:<asad>@ds047945.mlab.com:47945/todoapi');
 //as mongoose by default uses callbacks but we are using here promises so set promises global for mongoose
 
 module.exports={mongoose};
