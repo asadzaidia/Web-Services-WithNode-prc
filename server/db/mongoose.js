805d282079610dab885b93b84f68ff17b9896577
asadzaidia <asadzaidi625@gmail.com>
2018-01-06 13:27:43 +0500

    n

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://<asad>:<asad>@ds047945.mlab.com:47945/todoapi'||'mongodb://localhost:27017/TodoAppApi');
+mongoose.connect('mongodb://<asad>:<asad>@ds047945.mlab.com:47945/todoapi');
 //as mongoose by default uses callbacks but we are using here promises so set promises global for mongoose
 
 module.exports={mongoose};
+//||'mongodb://localhost:27017/TodoAppApi
